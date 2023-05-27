eaf79840a1558b76bd4adc9e20c8838dfb02e172
nitipon <nitipon.apaisri@gmail.com>
2023-05-27 21:39:04 +0200

    Added an illustration for page not found

--- src/views/pageNotFound/PageNotFoundView.tsx
+++ src/views/pageNotFound/PageNotFoundView.tsx
@@ -1,7 +1,9 @@
+import PageNotFoundImg from "../../assets/images/page_not_found.svg";
 const PageNotFoundView = () => {
     return (
-        <div>
-            <h1>Page not found</h1>
+        <div className="page-not-found-img">
+            <img src={PageNotFoundImg} alt="Page not found" />
+            <a href="https://storyset.com/web">Web illustrations by Storyset</a>
         </div>
     );
 };
