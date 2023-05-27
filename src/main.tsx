cc094814f94e49f48e51f8fe22f6181086ce4749
nitipon <nitipon.apaisri@gmail.com>
2023-05-27 20:11:54 +0200

    Added a route for an error(404)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import HomeView from "./views/home/HomeView";
 import AppLayout from "./layout/Layout";
 import AccountView from "./views/account/AccountView";
+import PageNotFoundView from "./views/pageNotFound/PageNotFoundView";
 
 const router = createBrowserRouter([
-    { path: "/", element: <HomeView /> },
+    { path: "/", element: <HomeView />, errorElement: <Navigate to="pageNotFound" /> },
     { path: "/account/:address", element: <AccountView /> },
+    { path: "/pageNotFound", element: <PageNotFoundView /> },
 ]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
